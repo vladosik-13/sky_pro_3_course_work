f5b1a5f773279aad91890064a85043ee1aa1509c
Владислав Кошелев <testvlad59@gmail.com>
2024-10-10 23:39:53 +0300

    реализована функция которая принимает список транзакций и возвращает список последние 4 цифры карты; общая сумма расходов; кешбэк (1 рубль на каждые 100 рублей)

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,10 @@ from datetime import datetime
 import pandas as pd
 
 
+from collections import defaultdict
+from typing import List, Dict
+
+
 # Настройка логгера
 logger = logging.getLogger(__file__)
 logger.setLevel(logging.INFO)
@@ -61,12 +65,56 @@ def reading_file_from_excel(path_file: str):
         return None
 
 
+def get_cardmask_costs_and_cashback(transactions: List[Dict]) -> List[Dict]:
+    """Функция принимает список транзакций словарией и возвращает информацию по каждой карте:
+    последние 4 цифры номера карты, общая сумма расходов, кэшбек"""
+
+    logger.info("Функция начала свою работу.")
+
+    try:
+        if not transactions:
+            logger.warning("Список транзакций пуст.")
+            return []
+
+        expenditure_by_card = defaultdict(int)
+
+        logger.info("Функция обрабатывает данные транзакций.")
+
+        for transaction in transactions:
+            card_num = transaction.get("Номер карты")
+            amount = transaction.get("Сумма операции", 0)
+            if card_num:
+                expenditure_by_card[card_num] += amount
+
+        result_transaction_list = []
+
+        logger.info("Функция формирует итоговый результат.")
+
+        for card_num, total_spent in expenditure_by_card.items():
+            result_transaction_list.append(
+                {
+                    "last_digits": card_num[-4:],
+                    "total_spent": round(total_spent, 2),
+                    "cashback": round(total_spent / 100, 2),
+                }
+            )
+        logger.info("Функция успешно завершила свою работу.")
+
+        return result_transaction_list
+
+    except Exception as e:
+        logger.error(f"Произошла ошибка: {e}")
+        return []
+
+
 if __name__ == "__main__":
     date_input = "2024-10-23 14:30:00"
     print(greetings(date_input))
 
     excel_path = 'C:/Users/user/PycharmProjects/sky_pro_4_course_work/data/operations.xlsx'
     transactions_df = reading_file_from_excel(excel_path)
-    if transactions_df is not None:
-        print(transactions_df.head())  # Печатает первые 5 строк DataFrame
 
+    if transactions_df is not None:
+        transactions = transactions_df.to_dict(orient='records')
+        card_info = get_cardmask_costs_and_cashback(transactions)
+        print(card_info)
