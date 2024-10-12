d9624d609175d678e0f3027e03d189d725e7961e
Владислав Кошелев <testvlad59@gmail.com>
2024-10-13 00:18:49 +0300

    реализована функция "investment_bank" которая ычисляет общую сумму инвестиций за указанный месяц с учетом лимита округления

--- src/services.py
+++ src/services.py
@@ -8,10 +8,9 @@ from config import SERVICES_LOGS
 from src.utils import reading_file_from_excel
 
 
-# Настройка логирования
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler("services_logs", mode='w')
+file_handler = logging.FileHandler(SERVICES_LOGS, mode='w')
 formatter = logging.Formatter('%(asctime)s - %(filename)s - %(funcName)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
@@ -49,3 +48,27 @@ def filter_transactions_by_month(month: str, transactions: List[Dict[str, Any]])
 
     logger.info("Завершение работы функции filter_transactions_by_month.")
     return filtered_transactions
+
+
+def investment_bank(month: str, transactions: List[Dict[str, Any]], limit: int) -> str:
+    """ функция ычисляет общую сумму инвестиций за указанный месяц с учетом лимита округления."""
+    logger.info("Начало работы функции calculate_investment.")
+
+    investment_sum = 0
+    filtered_transactions = filter_transactions_by_month(month, transactions)
+
+    for transaction in filtered_transactions:
+        rounded_value = round_payment(limit, transaction["Сумма операции"])
+        investment_sum += abs(rounded_value) - abs(transaction["Сумма операции"])
+
+    logger.info("Завершение работы функции calculate_investment.")
+    return json.dumps(round(investment_sum, 2), ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    try:
+        data_from_excel = reading_file_from_excel("operations.xls")
+        result = investment_bank("2021-10", data_from_excel.to_dict(orient="records"), 100)
+        print(result)
+    except Exception as e:
+        logger.error("Ошибка при выполнении программы: %s", e)
